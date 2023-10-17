e6860c7426077041e45c5c7f2f9c6f361c36ebf9
Raghav Kavimandan <raghavkavimandan23@gmail.com>
2023-10-17 13:05:59 +0530

    added 2 graph questions

---
+++ DSA_Practice/DSA_Practice/src/Graphs/City_with_Smallest_Neighbours.java
@@ -0,0 +1,55 @@
+package Graphs;
+
+public class City_with_Smallest_Neighbours {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        int m = edges.length;
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            dist[u][v] = wt;
+            dist[v][u] = wt;
+        }
+
+        for (int i = 0; i <n ; i++) {
+            dist[i][i] = 0;
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(dist[i][k] == Integer.MAX_VALUE || dist[k][j] == Integer.MAX_VALUE ){
+                        continue;
+                    }
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int countCity = n;
+        int currCity = -1;
+
+        for (int city = 0; city < n; city++) {
+            int count = 0;
+            for (int adjCity = 0; adjCity < n; adjCity++) {
+                if(dist[city][adjCity] <= distanceThreshold){
+                    count++;
+                }
+            }
+
+            if(count <= countCity){
+                countCity = count;
+                currCity = city;
+            }
+        }
+        return currCity;
+    }
+}
